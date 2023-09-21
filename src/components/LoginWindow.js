4505978ffa2889fcc5b433ed88ecc699b94ced14
AlbertB93 <bercik93@interia.eu>
2023-09-21 14:53:41 +0200

    New Login Form.

--- src/components/LoginWindow.js
+++ src/components/LoginWindow.js
@@ -3,12 +3,36 @@ import "../LoginWindow.css";
 
 class LoginWindow extends React.Component {
   render() {
+    function restorePage() {
+      window.location.replace("http://wp.pl");
+    }
+
     return (
       <div>
-        <label> Login </label>
-        <input type="text" className="loginInput" />
-        <label> Hasło </label>
-        <input type="text" className="loginInput" />
+        <form
+          className="login_form"
+          method="post"
+          name="loginForm" /*  action="../home.html" */
+        >
+          <label> Login </label>
+          <input
+            type="text"
+            className="loginInput"
+            placeholder="Tutaj wpisz login"
+            name="login"
+          />
+          <label> Hasło </label>
+          <input
+            type="text"
+            className="loginInput"
+            placeholder="Tutaj wpisz hasło"
+            name="password"
+          />
+          <button type="submit" id="login-btn" onClick={restorePage}>
+            {" "}
+            Zaloguj{" "}
+          </button>
+        </form>
       </div>
     );
   }
